297b56dafab3fa25f51089e74f66494fa93ba34d
Viresh Kumar <viresh.linux@gmail.com>
2012-06-20 12:53:02 -0700

    Viresh has moved
    viresh.kumar@st.com email-id doesn't exist anymore as I have left the
    company.  Replace ST's id with viresh.linux@gmail.com.
    
    It also updates .mailmap file to fix address for 'git shortlog'
    
    Signed-off-by: Viresh Kumar <viresh.linux@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Conflicts:
    	.mailmap
    	Documentation/arm/SPEAr/overview.txt
    	MAINTAINERS
    	arch/arm/boot/dts/spear1310-evb.dts
    	arch/arm/boot/dts/spear1310.dtsi
    	arch/arm/boot/dts/spear1340-evb.dts
    	arch/arm/boot/dts/spear1340.dtsi
    	arch/arm/boot/dts/spear13xx.dtsi
    	arch/arm/boot/dts/spear300-evb.dts
    	arch/arm/boot/dts/spear300.dtsi
    	arch/arm/boot/dts/spear310-evb.dts
    	arch/arm/boot/dts/spear310.dtsi
    	arch/arm/boot/dts/spear320-evb.dts
    	arch/arm/boot/dts/spear320.dtsi
    	arch/arm/boot/dts/spear3xx.dtsi
    	arch/arm/mach-spear13xx/include/mach/debug-macro.S
    	arch/arm/mach-spear13xx/include/mach/dma.h
    	arch/arm/mach-spear13xx/include/mach/generic.h
    	arch/arm/mach-spear13xx/include/mach/gpio.h
    	arch/arm/mach-spear13xx/include/mach/irqs.h
    	arch/arm/mach-spear13xx/include/mach/spear.h
    	arch/arm/mach-spear13xx/include/mach/timex.h
    	arch/arm/mach-spear13xx/include/mach/uncompress.h
    	arch/arm/mach-spear13xx/spear1310.c
    	arch/arm/mach-spear13xx/spear1340.c
    	arch/arm/mach-spear13xx/spear13xx.c
    	arch/arm/mach-spear3xx/spear300.c
    	arch/arm/mach-spear3xx/spear310.c
    	arch/arm/mach-spear3xx/spear320.c
    	arch/arm/mach-spear3xx/spear3xx.c
    	arch/arm/plat-spear/include/plat/pl080.h
    	arch/arm/plat-spear/pl080.c
    	drivers/pinctrl/spear/pinctrl-spear.c
    	drivers/pinctrl/spear/pinctrl-spear.h
    	drivers/pinctrl/spear/pinctrl-spear1310.c
    	drivers/pinctrl/spear/pinctrl-spear1340.c
    	drivers/pinctrl/spear/pinctrl-spear300.c
    	drivers/pinctrl/spear/pinctrl-spear310.c
    	drivers/pinctrl/spear/pinctrl-spear320.c
    	drivers/pinctrl/spear/pinctrl-spear3xx.c
    	drivers/pinctrl/spear/pinctrl-spear3xx.h
    	drivers/watchdog/sp805_wdt.c

--- arch/arm/mach-spear3xx/spear300.c
+++ arch/arm/mach-spear3xx/spear300.c
@@ -3,8 +3,8 @@
  *
  * SPEAr300 machine source file
  *
- * Copyright (C) 2009 ST Microelectronics
- * Viresh Kumar<viresh.kumar@st.com>
+ * Copyright (C) 2009-2012 ST Microelectronics
+ * Viresh Kumar <viresh.linux@gmail.com>
  *
  * This file is licensed under the terms of the GNU General Public
  * License version 2. This program is licensed "as is" without any
