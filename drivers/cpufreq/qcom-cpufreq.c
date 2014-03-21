5d103f2df7e099bbf59584d2737a44244a700a67
Vikram Mulukutla <markivx@codeaurora.org>
2014-03-20 20:14:57 -0700

    qcom-cpufreq: Move the enabling of the CPU/L2 clocks to CPU_STARTING
    It is a requirement of PLLs in MSM SoCs that the PLL power
    supplies be enabled while the PLL is running. If the PLL is
    locked and outputs enabled, turning off the regulator(s)
    supplying the PLL will cause the PLL to enter an unpredictable
    state.
    
    Now in the CPU_UP_PREPARE notifier, the CPU clocks are prepared
    and enabled, causing the source HFPLLs to also turn on. Note
    that the CPU isn't clocked yet. It is possible that execution is
    pre-empted and the CPU running the notifier enters power
    collapse. If all other CPUs also enter power collapse, then it
    is possible for an RPM notification to go out, allowing the RPM
    to transition the Apps processor to its sleep set. This can
    result in the HFPLL supplies being turned off while the HFPLL is
    running, violating the requirement mentioned above. Once the CPU
    is unclamped, the CPU is effectively unclocked, due to the HFPLL
    being in an unknown state.
    
    There is a check that is enabled in the PM code's CPU_UP_PREPARE
    notifier callback. This check ensures that the problematic RPM
    notification cannot occur until the core that is being brought
    online also enters power collapse. However, there is no ordering
    guarantee between that PM's hotplug notifier callback's execution
    and the cpufreq hotplug notifier callback's execution. This
    ordering depends on program link order, which is unreliable.
    
    It is necessary to ensure that once the HFPLL is enabled, the
    RPM cannot transition apps to its sleep set. Move the enabling
    of the CPU clocks to the CPU_STARTING notifier, which runs on
    the CPU bring brought online. The CPU_STARTING notifier is
    guaranteed to run after the CPU_UP_PREPARE notifier, which
    implies that the aforementioned do-not-notify-rpm check is
    executed *before* the HFPLL is enabled. Therefore even if all
    cores enter power collapse after the HFPLL is enabled, the
    HFPLL supplies are guaranteed to stay on, or the CPU clock
    is switched to the safe source and the HFPLL is turned off.
    
    CRs-Fixed: 622738
    Change-Id: I136841405806c07e755919859e649ded0c719abb
    Signed-off-by: Vikram Mulukutla <markivx@codeaurora.org>
    
    Conflicts:
    	drivers/cpufreq/qcom-cpufreq.c

--- drivers/cpufreq/qcom-cpufreq.c
+++ drivers/cpufreq/qcom-cpufreq.c
@@ -503,21 +503,27 @@ static int __cpuinit msm_cpufreq_cpu_callback(struct notifier_block *nfb,
 			if (rc < 0)
 				return NOTIFY_BAD;
 			rc = clk_prepare(cpu_clk[cpu]);
-			if (rc < 0)
+			if (rc < 0) {
+				clk_unprepare(l2_clk);
 				return NOTIFY_BAD;
+			}
 			update_l2_bw(&cpu);
 		}
 		break;
+
 	case CPU_STARTING:
 		if (is_clk) {
 			rc = clk_enable(l2_clk);
 			if (rc < 0)
 				return NOTIFY_BAD;
 			rc = clk_enable(cpu_clk[cpu]);
-			if (rc < 0)
+			if (rc) {
+				clk_disable(l2_clk);
 				return NOTIFY_BAD;
+			}
 		}
 		break;
+
 	default:
 		break;
 	}
