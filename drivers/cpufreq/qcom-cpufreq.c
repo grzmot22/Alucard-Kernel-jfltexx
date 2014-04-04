942eee96aa32fbf3bb67cfba7eb9b56a0baa246b
Junjie Wu <junjiew@codeaurora.org>
2014-04-04 12:30:00 -0700

    qcom-cpufreq: Add support for separate governor tunables per policy
    CPUs under different policies might need different governor tunables
    on certain targets. Enable support for this feature by parsing
    "qcom,governor-per-policy" DT property.
    
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    (+)
    
    cpufreq: Move governor-per-policy parsing before parsing freq table
    Even if all CPUs share same frequency table, there might still be use
    case where governor-per-policy is useful. Move it before returning
    from parsing common CPU freq table.
    
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/cpufreq/qcom-cpufreq.c
+++ drivers/cpufreq/qcom-cpufreq.c
@@ -596,6 +596,10 @@ static int __init msm_cpufreq_probe(struct platform_device *pdev)
 			is_sync = true;
 	}
 
+	/* Use per-policy governor tunable for some targets */
+	if (of_property_read_bool(dev->of_node, "qcom,governor-per-policy"))
+		msm_cpufreq_driver.flags |= CPUFREQ_HAVE_GOVERNOR_PER_POLICY;
+
 	if (!cpu_clk[0])
 		return -ENODEV;
 
