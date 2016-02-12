dc869435d6549d8e15613b11621b99a0a487ef0a
Alucard24 <dmbaoh2@gmail.com>
2016-02-12 12:58:30 +0100

    staging sync with @dorimanx master branch!

--- drivers/staging/android/ram_console.c
+++ drivers/staging/android/ram_console.c
@@ -13,6 +13,8 @@
  *
  */
 
+#define pr_fmt(fmt) "ram_console: " fmt
+
 #include <linux/console.h>
 #include <linux/init.h>
 #include <linux/module.h>
@@ -28,6 +30,10 @@ static struct persistent_ram_zone *ram_console_zone;
 static const char *bootinfo;
 static size_t bootinfo_size;
 
+/* LGE : not use Reed-solmon ECC check for ram-console data integrity.
+  * this ECC use cpu-power intensively that lead  slow down a boot time
+  */
+#define USE_RAM_CONSOLE_ECC	false
 static void
 ram_console_write(struct console *console, const char *s, unsigned int count)
 {
@@ -55,7 +61,7 @@ static int __devinit ram_console_probe(struct platform_device *pdev)
 	struct ram_console_platform_data *pdata = pdev->dev.platform_data;
 	struct persistent_ram_zone *prz;
 
-	prz = persistent_ram_init_ringbuffer(&pdev->dev, true);
+	prz = persistent_ram_init_ringbuffer(&pdev->dev, USE_RAM_CONSOLE_ECC);
 	if (IS_ERR(prz))
 		return PTR_ERR(prz);
 
@@ -163,7 +169,7 @@ static int __init ram_console_late_init(void)
 
 	entry = create_proc_entry("last_kmsg", S_IFREG | S_IRUGO, NULL);
 	if (!entry) {
-		printk(KERN_ERR "ram_console: failed to create proc entry\n");
+		pr_err("failed to create proc entry\n");
 		persistent_ram_free_old(prz);
 		return 0;
 	}
